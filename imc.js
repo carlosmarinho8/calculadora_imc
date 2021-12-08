b36ea4fd3f08b68195202863d9a50c0e4d9a2f21
Carlos Marinho <81977958+carlosmarinho8@users.noreply.github.com>
2021-12-08 09:03:26 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,51 @@
+const calcular = document.getElementById('calcular');
+
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') 
+    {
+        
+        const valorIMC = (peso / (altura ** 2)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5)
+        {
+            classificacao = 'abaixo do peso.';
+        }
+        else if (valorIMC < 25)
+        {
+            classificacao = 'com peso normal';
+        }
+        else if (valorIMC < 30)
+        {
+            classificacao = 'no sobrepeso.';
+        }
+        else if (valorIMC < 35)
+        {
+            classificacao = 'na obesidade grau I.';
+        }
+        else if (valorIMC < 40)
+        {
+            classificacao = 'na obesidade grau II';
+        }
+        else
+        {
+            classificacao = 'na obesidade grau III. Cuidado!!';
+        }
+
+            resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+        }
+    else 
+    {
+            resultado.textContent = 'Preencha todos os campos!!!';
+    }
+        
+}
+
+calcular.addEventListener('click', imc);
